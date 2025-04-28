376df3fc584dd6c04e998ad2d9af54111db296e0
nora a-rahim <119633500+neraeura@users.noreply.github.com>
2025-04-28 13:56:56 -0400

    fixed login.php Oauth url

--- login.php
+++ login.php
@@ -1,22 +1,19 @@
 <?php
-// login.php
-
-// Configuration
 $client_id = '680845439269-82o88qm1ibcjlsnul3smgg1est9dhv9o.apps.googleusercontent.com';
 $redirect_uri = 'https://noraa.sgedu.site/finalTest/redirect.php';
 
-// Scopes you are asking from user
+
+// scope: A space-delimited list of scopes that identify the 
+// resources that your application could access on the user's behalf. 
+// These values inform the consent screen that Google displays to the user.
 $scope = 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
 
 // Build the Google OAuth URL
 $auth_url = "https://accounts.google.com/o/oauth2/v2/auth?" . http_build_query([
-    'client_id' => $client_id,
-    'redirect_uri' => $redirect_uri,
-    'response_type' => 'code',
     'scope' => $scope,
-    'access_type' => 'offline',            
-    'prompt' => 'consent',                  // Force Google to show consent screen again
-    'include_granted_scopes' => 'true'      // (Optional) allow future scope expansion
+    'response_type' => 'code',
+    'redirect_uri' => $redirect_uri,
+    'client_id' => $client_id
 ]);
 
 // Redirect user to Google OAuth URL
